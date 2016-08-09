f649060d587679e67ee242c5eebca534a7f1a788
shwinpiocess <springwind_lee@163.com>
2016-08-09 13:39:52 +0800

    使用路由

--- app/view/main/MainViewController.js
+++ app/view/main/MainViewController.js
@@ -17,8 +17,93 @@ Ext.define('Admin.view.main.MainViewController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.main.main',
 
-    onMainViewRender: function(component, eOpts) {
+    requires: [
+        'Ext.app.route.Route'
+    ],
 
+    config: {
+        lastView: 'null'
+    },
+
+    listen: {
+        controller: {
+            '#': {
+                unmatchedroute: 'onRouteChange'
+            }
+        }
+    },
+
+    routes: {
+        ':node': 'onRouteChange'
+    },
+
+    onRouteChange: function(node) {
+        this.setCurrentView(id);
+    },
+
+    setCurrentView: function(hashTag) {
+                hashTag = (hashTag || '').toLowerCase();
+
+                var me = this,
+                    refs = me.getReferences(),
+                    mainCard = refs.mainCardPanel,
+                    mainLayout = mainCard.getLayout(),
+                    navigationList = refs.navigationTreeList,
+                    store = navigationList.getStore(),
+                    node = store.findNode('routeId', hashTag) ||
+                           store.findNode('viewType', hashTag),
+                    view = (node && node.get('viewType')) || 'page404',
+                    lastView = me.lastView,
+                    existingItem = mainCard.child('component[routeId=' + hashTag + ']'),
+                    newView;
+
+                // Kill any previously routed window
+                if (lastView && lastView.isWindow) {
+                    lastView.destroy();
+                }
+
+                lastView = mainLayout.getActiveItem();
+
+                if (!existingItem) {
+                    newView = Ext.create({
+                        xtype: view,
+                        routeId: hashTag,  // for existingItem search later
+                        hideMode: 'offsets'
+                    });
+                }
+
+                if (!newView || !newView.isWindow) {
+                    // !newView means we have an existing view, but if the newView isWindow
+                    // we don't add it to the card layout.
+                    if (existingItem) {
+                        // We don't have a newView, so activate the existing view.
+                        if (existingItem !== lastView) {
+                            mainLayout.setActiveItem(existingItem);
+                        }
+                        newView = existingItem;
+                    }
+                    else {
+                        // newView is set (did not exist already), so add it and make it the
+                        // activeItem.
+                        Ext.suspendLayouts();
+                        mainLayout.setActiveItem(mainCard.add(newView));
+                        Ext.resumeLayouts(true);
+                    }
+                }
+
+                navigationList.setSelection(node);
+
+                if (newView.isFocusable(true)) {
+                    newView.focus();
+                }
+
+                me.lastView = newView;
+    },
+
+    onMainViewRender: function(component, eOpts) {
+        if (!window.location.hash) {
+            this.redirectTo("dashboard");
+        }
     },
 
     onToggleNavigationSize: function(button, e) {
